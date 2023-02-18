81e98d8697dcb6e1fc060a6fee55733c30e8c172
Kevin Greer <kmgreer405@hotmail.com>
2023-02-18 13:38:24 -0500

    Last push. attempted first 2 in bonus.

--- scripts/prescribers_project.sql
+++ scripts/prescribers_project.sql
@@ -101,6 +101,14 @@ FROM drug
 GROUP BY generic_name
 ORDER BY SUM(total_drug_cost) DESC;
 
+SELECT generic_name,
+ROUND(SUM(total_drug_cost)/total_day_supply, 2) AS cost_per_day
+FROM drug
+	INNER JOIN prescription
+	USING (drug_name)
+GROUP BY generic_name, total_day_supply
+ORDER BY SUM(total_drug_cost) DESC;
+
 --Insulin Glargine has the highest drug cost per day at 285,654.98 monies a day.
 
 -- 4. 
@@ -238,3 +246,64 @@ AND nppes_provider_city = 'NASHVILLE'
 AND opioid_drug_flag = 'Y'
 GROUP BY p.npi, d.drug_name
 ORDER BY SUM(p2.total_claim_count) DESC;
+
+--BONUS
+-- 1. How many npi numbers appear in the prescriber table but not in the prescription table?
+SELECT COUNT(p1.npi)-COUNT(p2.npi) AS npi_diff
+FROM prescriber AS p1
+	FULL JOIN prescription AS p2
+	USING(npi);
+
+--There are 4,458 more npi's in the prescriber table compared to the prescription table.
+
+-- 2.
+--     a. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Family Practice.
+SELECT generic_name,
+COUNT(generic_name)
+FROM drug
+	CROSS JOIN prescriber
+WHERE specialty_description = 'Family Practice'
+GROUP BY generic_name
+ORDER BY COUNT(generic_name) DESC
+LIMIT 5;
+
+
+--     b. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Cardiology.
+SELECT generic_name,
+COUNT(generic_name)
+FROM drug
+	CROSS JOIN prescriber
+WHERE specialty_description = 'Cardiology'
+GROUP BY generic_name
+ORDER BY COUNT(generic_name) DESC
+LIMIT 5;
+
+--     c. Which drugs are in the top five prescribed by Family Practice prescribers and Cardiologists? Combine what you did for parts a and b into a single query to answer this question.
+SELECT generic_name,
+COUNT(generic_name)
+FROM drug
+	CROSS JOIN prescriber
+WHERE specialty_description = 'Family Practice'
+GROUP BY generic_name
+UNION
+SELECT generic_name,
+COUNT(generic_name)
+FROM drug
+	CROSS JOIN prescriber
+WHERE specialty_description = 'Cardiology'
+GROUP BY generic_name
+ORDER BY COUNT(generic_name) DESC
+LIMIT 5;
+-- 3. Your goal in this question is to generate a list of the top prescribers in each of the major metropolitan areas of Tennessee.
+--     a. First, write a query that finds the top 5 prescribers in Nashville in terms of the total number of claims (total_claim_count) across all drugs. Report the npi, the total number of claims, and include a column showing the city.
+    
+--     b. Now, report the same for Memphis.
+    
+--     c. Combine your results from a and b, along with the results for Knoxville and Chattanooga.
+
+-- 4. Find all counties which had an above-average number of overdose deaths. Report the county name and number of overdose deaths.
+
+-- 5.
+--     a. Write a query that finds the total population of Tennessee.
+    
+--     b. Build off of the query that you wrote in part a to write a query that returns for each county that county's name, its population, and the percentage of the total population of Tennessee that is contained in that county.
